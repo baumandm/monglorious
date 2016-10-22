f15eb793f5a81fe9a480ea3658251aaf39bf8036
Dave Bauman <baumandm@gmail.com>
2016-10-22 17:32:42 -0400

    Attempt to switch to Midje without breaking anything

--- project.clj
+++ project.clj
@@ -11,4 +11,7 @@
 
   :main ^:skip-aot monglorious.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2.1"]]}
+             :uberjar {:aot :all}})
--- test/monglorious/core_test.clj
+++ test/monglorious/core_test.clj
@@ -1,7 +1,9 @@
 (ns monglorious.core-test
-  (:require [clojure.test :refer :all]
-            [monglorious.core :refer :all]))
+  (:require [monglorious.core :refer :all]
+            [clojure.string :as str])
+  (:use midje.sweet)  )
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(fact "`split` splits strings on regular expressions and returns a vector"
+      (str/split "a/b/c" #"/") => ["a" "b" "c"]
+      (str/split "" #"irrelevant") => [""]
+      (str/split "no regexp matches" #"a+\s+[ab]") => ["no regexp matches"])
