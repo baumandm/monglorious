2828f4c5a3606050fa52ef14595e6dcf9cfad312
Dave Bauman <baumandm@gmail.com>
2016-10-22 17:45:15 -0400

    Add some legit tests

--- test/monglorious/core_test.clj
+++ test/monglorious/core_test.clj
@@ -3,7 +3,13 @@
             [clojure.string :as str])
   (:use midje.sweet)  )
 
-(fact "`split` splits strings on regular expressions and returns a vector"
-      (str/split "a/b/c" #"/") => ["a" "b" "c"]
-      (str/split "" #"irrelevant") => [""]
-      (str/split "no regexp matches" #"a+\s+[ab]") => ["no regexp matches"])
+(fact "Monglorious accepts SHOW ___ commands"
+      (monglorious-parser "show dbs") => [:command [:show-command [:dbs]]]
+      (monglorious-parser "show collections") => [:command [:show-command [:collections]]]
+      (monglorious-parser "show users") => [:command [:show-command [:users]]]
+      (monglorious-parser "show roles") => [:command [:show-command [:roles]]]
+      (monglorious-parser "show profile") => [:command [:show-command [:profile]]]
+      (monglorious-parser "show databases") => [:command [:show-command [:databases]]])
+
+(fact "Monglorious accepts DB Collection commands"
+      (monglorious-parser "db.fooCollection.count()") => [:command [:collection-command "fooCollection" "count"]])
