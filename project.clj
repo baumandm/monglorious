f61f6f28637baf22dcd565701d54bd4b8d53e15f
Dave Bauman <baumandm@gmail.com>
2016-11-02 01:13:32 -0400

    Bump version number

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject monglorious "0.1.0-SNAPSHOT"
+(defproject monglorious "0.1.0"
   :author "Dave Bauman"
   :description "Query MongoDB using strings!"
   :url "https://github.com/baumandm/monglorious"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"
+  :license {:name         "Eclipse Public License"
+            :url          "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
@@ -15,11 +15,11 @@
   :target-path "target/%s"
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
 
-  :profiles {:dev {:dependencies [[midje "1.8.3"]
-                                  [org.slf4j/slf4j-nop "1.7.12"]]
-                   :plugins [[lein-midje "3.2.1"]
-                             [lein-kibit "0.1.2"]
-                             [jonase/eastwood "0.2.3"]]}
+  :profiles {:dev     {:dependencies [[midje "1.8.3"]
+                                      [org.slf4j/slf4j-nop "1.7.12"]]
+                       :plugins      [[lein-midje "3.2.1"]
+                                      [lein-kibit "0.1.2"]
+                                      [jonase/eastwood "0.2.3"]]}
 
              :uberjar {:aot :all}}
 
@@ -34,4 +34,5 @@
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]]
 
-  :deploy-repositories [["releases" :clojars]])
+  :deploy-repositories [["clojars" {:url "https://clojars.org/monglorious"
+                              :creds :gpg}]])
